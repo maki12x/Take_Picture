d75e2b97feaa11f399bc248d437c8abdcc523146
maki12x <sulewskimaciek@gmail.com>
2018-10-23 21:27:19 +0200

    Added hough circle detection and Morph_Open, code needs a lot of cleanup for proper testing

--- src/mavrosCommandMain.cpp
+++ src/mavrosCommandMain.cpp
@@ -4,19 +4,57 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
-
+#include <vector>
 
 using namespace std;
 using namespace cv;
-
-
-double pictureFrequency = 40; //40 = 1 photo per  two seconds
 
 int frequency = 20;
 int loopCounter = 0;
 
 
+const int alpha_slider_max = 255;
+int alpha_slider;
+double alpha;
+double beta;
+Mat image_open;
+Mat frame;
+Mat lower_red_hue_range, upper_red_hue_range, red_hue_image, hsv_image;
 
+void on_trackbar( int, void* )
+{
+	cvtColor(frame, hsv_image, COLOR_BGR2HSV);
+	
+	inRange(hsv_image, Scalar(0,100,90), Scalar(30,255,255) , lower_red_hue_range);
+	inRange(hsv_image, Scalar(140,100,90), Scalar(179,255,255) , upper_red_hue_range);
+	
+	addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
+	GaussianBlur(red_hue_image, red_hue_image, Size(9,9), 2, 2);
+	
+	int morph_size = 1;
+	Mat element = getStructuringElement( MORPH_ELLIPSE, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
+	morphologyEx(red_hue_image, image_open, MORPH_OPEN, element, Point(-1,-1), 3 ); 
+	
+	vector<Vec3f> circles;
+
+    HoughCircles( image_open, circles, CV_HOUGH_GRADIENT,1.2,image_open.rows/8,200, 50,0, 0 );
+	
+	cvtColor(image_open, image_open, COLOR_GRAY2BGR);
+	
+   // Draw the circles detected
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+         Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+         int radius = cvRound(circles[i][2]);
+         // circle center
+         circle( image_open, center,3, Scalar(0,0,255), -1, 8, 0 );
+         // circle outline
+         circle( image_open, center, radius, Scalar(0,0,255),5, 8, 0 );
+         cout << "Circle " << i << " center: " << center << " radius: " << radius << endl;
+    }
+	
+	imshow( "okno 1", image_open );
+}
 
 int main(int argc, char* argv[]){
 
@@ -26,7 +64,7 @@ int main(int argc, char* argv[]){
 	ros::Rate loop_rate(frequency);
 	sleep(1);
 		
-	VideoCapture cap(0);
+	VideoCapture cap(1);
 	
 	if (cap.isOpened() == false)  
 	{
@@ -34,45 +72,24 @@ int main(int argc, char* argv[]){
 		return -1;
 	} 
 	
-	int cntr = 0;
-	int rcValue = 0;
-	string name = get_username();
+	namedWindow( "okno 1", WINDOW_AUTOSIZE );
+	alpha_slider = 0;
+	
+	
+	char TrackbarName[50];
+	sprintf( TrackbarName, "Alpha x %d", alpha_slider_max );
+	
+	createTrackbar( TrackbarName, "okno 1", &alpha_slider, alpha_slider_max, on_trackbar );
 	
 	while (ros::ok()) 
 	{
-		Mat frame;
 		
-		bool bSuccess = cap.read(frame);
-		if (bSuccess == false) 
-		{
-			cout << "Video camera is disconnected" << endl;
-		}
+		cap.read(frame);
 		
-		rcValue = command.getRCInput();
+		on_trackbar( alpha_slider, 0 );
 		
-		if(loopCounter >= pictureFrequency & rcValue >= 1500)
-		{
-			string savingName = "/home/" + name + "/zdj/" + to_string(cntr) + ".jpg";
-			imwrite(savingName, frame);
-			cout << "PICTURE: " << cntr << " TAKEN" << endl;
-			
-			Mat bwFrame;
-			
-			cvtColor(frame, bwFrame, COLOR_BGR2HSV);
-			Mat lower_red_hue_range;
-			Mat upper_red_hue_range;
-			inRange(bwFrame, Scalar(0,100,100), Scalar(10,255,255) , lower_red_hue_range);
-			inRange(bwFrame, Scalar(160,100,100), Scalar(179,255,255) , upper_red_hue_range);
-			Mat red_hue_image;
-			addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
-			GaussianBlur(red_hue_image, red_hue_image, Size(9,9), 2, 2);
-			savingName = "/home/" + name + "/zdj2/" + to_string(cntr) + ".jpg";
-			imwrite(savingName, red_hue_image);
-			cntr++;
-			loopCounter = 0;
-		}
+		waitKey(10);
 		
-		loopCounter++;
 		ros::spinOnce();
 		loop_rate.sleep();
 	}	
